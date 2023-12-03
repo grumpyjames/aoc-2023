53b1b5733fe5e5bd666f7622bbed1ab1146a21e5
James Byatt <grumpyjames@gmail.com>
2023-12-02 23:57:37 -0500

    Scaffolding for three

---
+++ src/main/java/net/digihippo/aoc/Three.java
@@ -0,0 +1,33 @@
+package net.digihippo.aoc;
+
+public class Three extends SolutionTemplate<Integer, Integer> {
+    @Override
+    Solution<Integer> partOne() {
+        return new Solution<>() {
+            @Override
+            public Integer result() {
+                return 2;
+            }
+
+            @Override
+            public void accept(String s) {
+
+            }
+        };
+    }
+
+    @Override
+    Solution<Integer> partTwo() {
+        return new Solution<>() {
+            @Override
+            public Integer result() {
+                return 5;
+            }
+
+            @Override
+            public void accept(String s) {
+
+            }
+        };
+    }
+}
---
+++ src/test/java/net/digihippo/aoc/ThreeTest.java
@@ -0,0 +1,10 @@
+package net.digihippo.aoc;
+
+class ThreeTest extends TestTemplate<Integer, Integer> {
+    private static final String EXAMPLE_INPUT = """
+            """;
+
+    public ThreeTest() {
+        super(new Three(), EXAMPLE_INPUT, 400, 31, "three.txt");
+    }
+}
