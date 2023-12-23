bdf22cb15b597a05fa286d710760e558a940beeb
James Byatt <grumpyjames@gmail.com>
2023-12-23 15:48:49 -0500

    OK apparently this equivalent program is not equivalent but works. Ugh

--- src/main/java/net/digihippo/aoc/TwentyThree.java
+++ src/main/java/net/digihippo/aoc/TwentyThree.java
@@ -9,11 +9,14 @@ public class TwentyThree extends GridSolution<Integer> {
         private TwoDPoint head = null;
         private final Set<TwoDPoint> visited = new HashSet<>();
         private final int maxY;
+        private final TwoDPoint start;
         private boolean chance = true;
         private boolean complete = false;
 
-        public PathSoFar(int maxY) {
+        public PathSoFar(int maxY, TwoDPoint start) {
             this.maxY = maxY;
+            this.start = start;
+            this.visited.add(start);
         }
 
         void append(TwoDPoint point) {
@@ -41,7 +44,7 @@ public class TwentyThree extends GridSolution<Integer> {
         }
 
         public PathSoFar copy() {
-            PathSoFar pathSoFar = new PathSoFar(maxY);
+            PathSoFar pathSoFar = new PathSoFar(maxY, start);
 
             pathSoFar.head = this.head;
             pathSoFar.visited.addAll(this.visited);
@@ -61,57 +64,144 @@ public class TwentyThree extends GridSolution<Integer> {
         public boolean worthContinuing() {
             return hasAChance() && !complete;
         }
+
+        public Hike toHike() {
+            return new Hike(start, head, visited.size());
+        }
     }
 
     @Override
     Integer partOne(Grid g) {
         return longestHike(g, (len, here, x, y, o, content) -> acceptable(o, content));
     }
+    record DecisionPoint(TwoDPoint decisionPoint, TwoDPoint pathToSuccess) {}
 
-    @Override
-    Integer partTwo(Grid g) {
-        final int accessible = g.count(c -> c != '#');
+    private static final class VertexPath
+    {
+        private final Set<TwoDPoint> visited;
+        private final TwoDPoint location;
+        private final int length;
 
-        final TwoDPoint finish = new TwoDPoint(g.columnCount() - 2, g.rowCount() - 1);
+        private VertexPath(TwoDPoint start, int length, Set<TwoDPoint> visited) {
+            this.location = start;
+            this.length = length;
+            this.visited = visited;
+            this.visited.add(start);
+        }
 
-        TwoDPoint prev = null;
-        TwoDPoint point = finish;
-        // track back until we find the first split.
-        final Set<TwoDPoint> visited = new HashSet<>();
-        final List<TwoDPoint> points = new ArrayList<>();
-        while (true) {
-            visited.add(point);
-            g.visitOrthogonalNeighboursOf(point.x(), point.y(), (o, x, y, content) -> {
-                TwoDPoint neigh = new TwoDPoint(x, y);
-                if (content != '#' && !visited.contains(neigh)) {
-                    points.add(neigh);
-                }
-            });
+        public boolean accepts(Hike hike) {
+            return !visited.contains(hike.alternateVertex(location));
+        }
 
-            if (points.size() > 1) {
-                break;
-            } else {
-                prev = point;
-                point = points.getFirst();
-                points.clear();
+        public VertexPath walk(Hike hike) {
+            final TwoDPoint dest = hike.alternateVertex(location);
+            final Set<TwoDPoint> visited = new HashSet<>(this.visited);
+
+            return new VertexPath(dest, length + hike.length, visited);
+        }
+
+        public boolean isAt(TwoDPoint finish) {
+            return finish.equals(location);
+        }
+
+        public int length() {
+            return length - visited.size() + 1;
+        }
+
+        @Override
+        public String toString() {
+            return "@" + location + " visited: " + visited.size();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            VertexPath that = (VertexPath) o;
+            return length == that.length && Objects.equals(location, that.location) && Objects.equals(visited, that.visited);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(visited, location, length);
+        }
+    }
+
+    int longestPath(
+            final Set<TwoDPoint> visited,
+            final Map<TwoDPoint, Set<Neighbour>> edgeMap,
+            final TwoDPoint from,
+            final TwoDPoint to,
+            final int distanceSoFar) {
+        if (from.equals(to)) {
+            return distanceSoFar;
+        }
+
+        visited.add(from);
+        int max = 0;
+
+        for (Neighbour neighbour : edgeMap.get(from)) {
+            final TwoDPoint destination = neighbour.dest;
+            if (!visited.contains(destination)) {
+                max = Math.max(max, longestPath(visited, edgeMap, destination, to, neighbour.distance - 1 + distanceSoFar));
             }
         }
 
-        final TwoDPoint decisionPoint = point;
-        final TwoDPoint pathToSuccess = prev;
-        return longestHike(g, (visitedSize, here, x, y, o, content) -> {
-            if (accessible - visitedSize < finish.manhattanDistance(here)) {
-                return false;
-            }
+        visited.remove(from);
 
+        return max;
+    }
 
-            boolean atDecisionPoint = decisionPoint.x() == here.x() && decisionPoint.y() == here.y();
-            if (atDecisionPoint) {
-                return pathToSuccess.x() == x && pathToSuccess.y() == y;
-            }
+    record Neighbour(TwoDPoint dest, int distance) {}
 
-            return content != '#';
+    @Override
+    Integer partTwo(Grid g) {
+        Set<Hike> hikes = new HashSet<>(hikes(g));
+
+        final Map<TwoDPoint, Set<Neighbour>> byVertex = new HashMap<>();
+        hikes.forEach(h -> {
+            byVertex.computeIfAbsent(h.from, twoDPoint -> new HashSet<>()).add(new Neighbour(h.to, h.length));
+            byVertex.computeIfAbsent(h.to, twoDPoint -> new HashSet<>()).add(new Neighbour(h.from, h.length));
         });
+
+        TwoDPoint start = new TwoDPoint(1, 0);
+        TwoDPoint finish = new TwoDPoint(g.columnCount() - 2, g.rowCount() - 1);
+        VertexPath vp = new VertexPath(start, 0, new HashSet<>());
+
+//        List<VertexPath> vps = new ArrayList<>();
+//        vps.add(vp);
+
+        HashSet<TwoDPoint> visited = new HashSet<>();
+        return longestPath(visited, byVertex, start, finish, 0);
+
+//        int longest = -1;
+//        while (!vps.isEmpty()) {
+//            final Set<VertexPath> newPaths = new HashSet<>();
+//            final Iterator<VertexPath> iterator = vps.iterator();
+//            while (iterator.hasNext()) {
+//                VertexPath here = iterator.next();
+//                iterator.remove();
+//
+//                if (here.isAt(finish)) {
+//                    if (longest < here.length()) {
+//                        longest = here.length();
+//                        System.out.println(Instant.now() + ": " + longest);
+//                    }
+//                } else {
+//                    Set<Hike> fromHere = byVertex.get(here.location);
+//                    for (Hike hike : fromHere) {
+//                        if (here.accepts(hike)) {
+//                            newPaths.add(here.walk(hike));
+//                        }
+//                    }
+//                }
+//            }
+//
+//            vps.addAll(newPaths);
+//        }
+
+        // 3962 ? too low still.
+//        return longest;
     }
 
     interface Worthy {
@@ -124,11 +214,78 @@ public class TwentyThree extends GridSolution<Integer> {
                 char content);
     }
 
+    record Hike(TwoDPoint from, TwoDPoint to, int length) {
+        public TwoDPoint alternateVertex(TwoDPoint location) {
+            return from.equals(location) ? to : from;
+        }
+    }
+
+    private List<Hike> hikes(Grid g) {
+        final List<Hike> result = new ArrayList<>();
+        final Set<TwoDPoint> vertices = new HashSet<>();
+
+        TwoDPoint start = new TwoDPoint(1, 0);
+
+        final List<PathSoFar> paths = new ArrayList<>();
+        final PathSoFar initial = new PathSoFar(g.rowCount() - 1, start);
+        initial.append(start);
+        paths.add(initial);
+
+        while (!paths.isEmpty()) {
+            final List<PathSoFar> newPaths = new ArrayList<>();
+
+            for (Iterator<PathSoFar> iterator = paths.iterator(); iterator.hasNext();) {
+                PathSoFar path = iterator.next();
+                TwoDPoint here = path.last();
+
+                final List<TwoDPoint> next = new ArrayList<>();
+                g.visitOrthogonalNeighboursOf(here.x(), here.y(), (o, x, y, content) -> {
+                    TwoDPoint neighbour = new TwoDPoint(x, y);
+                    if (!path.hasVisited(neighbour)) {
+                        if (content != '#') {
+                            next.add(neighbour);
+                        }
+                    }
+                });
+
+                if (next.isEmpty()) {
+                    result.add(path.toHike());
+                    iterator.remove();
+                } else {
+                    if (next.size() > 1) {
+                        result.add(path.toHike());
+                        iterator.remove();
+                        if (vertices.add(here)) {
+                            for (TwoDPoint twoDPoint : next) {
+                                final PathSoFar newPath = new PathSoFar(path.maxY, here);
+                                newPath.append(twoDPoint);
+                                newPaths.add(newPath);
+                            }
+                        }
+                    } else {
+                        path.append(next.getFirst());
+                    }
+                }
+            }
+
+            for (Iterator<PathSoFar> iterator = newPaths.iterator(); iterator.hasNext(); ) {
+                PathSoFar newPath = iterator.next();
+                if (newPath.complete) {
+                    result.add(newPath.toHike());
+                    iterator.remove();
+                }
+            }
+            paths.addAll(newPaths);
+        }
+
+        return result;
+    }
+
     private static int longestHike(Grid g, Worthy worthy) {
         TwoDPoint start = new TwoDPoint(1, 0);
 
         final List<PathSoFar> paths = new ArrayList<>();
-        final PathSoFar initial = new PathSoFar(g.rowCount() - 1);
+        final PathSoFar initial = new PathSoFar(g.rowCount() - 1, start);
         initial.append(start);
         paths.add(initial);
 
